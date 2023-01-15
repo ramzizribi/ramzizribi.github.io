155d6fa605e4d5ac7bc7a55d3b599aab0d6d3d6a
ramzizribi <122752599+ramzizribi@users.noreply.github.com>
2023-01-15 22:11:30 +0100

    Add files via upload

---
+++ projetweb/js/questions.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "Que signifie HTML ?",
+    choice1: "Hyperlinks and Text Markup Language",
+    choice2: "Hyper Text Markup Language",
+    choice3: "Hyper Text Making Language",
+    choice4: "Hyper Text Mark Language",
+    answer: 1
+},
+            {
+            question: "Que signifie CSS ?",
+    choice1: "Colorful StyleSheet",
+    choice2: "Creative Style Sheet",
+    choice3: "Cascading Style Sheet",
+    choice4: "Computer Style Sheet",
+    answer: 2
+},
+            {
+            question: "Quelle balise HTML est utilisée pour définir une feuille de style interne ?",
+    choice1: "<script>",
+    choice2: "<style>",
+    choice3: "<html>",
+    choice4: "<svg>",
+    answer: 1
+},
+            {
+            question: "Quelle est la bonne syntaxe CSS ?",
+    choice1: "body{color:black}",
+    choice2: "{body{color:black}",
+    choice3: "body={color:black}",
+    choice4: "body:color{black}",
+    answer: 0
+},
+            {
+            question: "Comment insérer un commentaire dans un fichier CSS ?",
+    choice1: "/*This is Comment*/",
+    choice2: "//This Is Comment",
+    choice3: "<!--- This Is Comment --->",
+    choice4: "//This Is Comment//",
+    answer: 1
+},
+            {
+            question: "Comment insérer un commentaire dans un fichier HTML ?",
+    choice1: "/*This is Comment*/",
+    choice2: "//This Is Comment",
+    choice3: "<!--- This Is Comment --->",
+    choice4: "//This Is Comment//",
+    answer: 2
+},
+            {
+               question: "Quelle propriété est utilisée pour changer la couleur d'arrière-plan ?",
+    choice1: "backgroundColor",
+    choice2: "BgColor",
+    choice3: "Color-Background",
+    choice4: "background",
+    answer: 3
+},{
+               question: "Comment écrire une instruction IF en JavaScript ?",
+    choice1: "if i==5",
+    choice2: "if(i==5)",
+    choice3: "if(i==5)then",
+    choice4: "if i==5 then",
+    answer: 2
+},
+            {
+               question: "Dans quel élément HTML place-t-on le JavaScript ?",
+    choice1: "<js></js>",
+    choice2: "<javascript></javascript>",
+    choice3: "<script></script>",
+    choice4: "<scripting>",
+    answer: 2
+},
+            {
+               question: "Comment démarre une boucle WHILE ?",
+    choice1: "while(i <= 0)",
+    choice2: "while(i <= 0 i++)",
+    choice3: "while i <= 0",
+    choice4: "while (i++ i <= 0)",
+    answer: 0
+}];
